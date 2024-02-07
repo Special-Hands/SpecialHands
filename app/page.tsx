f50284fea282a010878a3908b52ec168b58531d3
bryanpow <ramosbusiness544@gmail.com>
2024-02-06 22:49:42 -0500

    home work

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <div id='land' className='relative'>
-      <div className='absolute h-[100%] '>
-        <h1 className="text-white ">Save The World</h1>
+      <div id='land' className=' flex relative'>
+      <div className='absolute h-[100%] w-[100%] bg-black opacity-[50%]'>
+      </div>
+      <div className="absolute w-[60%] pl-[10%] pt-[10%] text-white">
+        <h1 >Together, let's reach out for change, one hand at a time</h1>
+        <p>Your support matters more than ever.</p>
       </div>
       <img src='/specialArtboard 1.svg'></img>
       </div>
