a1c28c5033c21b6c62748a261cf1de796e76319e
bryanpow <ramosbusiness544@gmail.com>
2024-02-06 19:02:11 -0500

    homeDraft

---
+++ components/NavBar.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import { BasicButton } from './Button'
+
+const NavBar = () => {
+  const links = [ 'About Us', 'Programs', 'Blog', 'Contact']
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default NavBar
