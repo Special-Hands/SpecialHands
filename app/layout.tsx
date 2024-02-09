7034e7ad272347185262adfad1cbe9379ebc101b
Brandon Ramos <bramos005@citymail.cuny.edu>
2024-02-09 15:37:34 -0500

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import NavBar from "@/app/components/NavBar";
+import NavBar from "./components/NavBar";
 import LetterHead from "@/app/components/LetterHead";
 import Footer from "@/app/components/Footer";
 
