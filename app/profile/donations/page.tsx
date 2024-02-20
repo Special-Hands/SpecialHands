26e6008434c47e9c474bf0f1d883fe4ac3825170
bryanpow <ramosbusiness544@gmail.com>
2024-02-20 18:57:10 -0500

    profile additions

--- app/profile/donations/page.tsx
+++ app/profile/donations/page.tsx
@@ -1,12 +1,17 @@
 'use client'
 import ProfileSideBar from '@/app/components/layout/ProfileSide'
 import UserDonations from '@/app/components/layout/UserDonations'
-import React from 'react'
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from 'react';
 
 function page() {
+  useEffect(() => {
+    Aos.init()
+}, [])
   return (
-    <div className='flex'>
-        <ProfileSideBar  />
+    <div data-aos='fade' className='flex'>
+
         <UserDonations />
     </div>
   )
