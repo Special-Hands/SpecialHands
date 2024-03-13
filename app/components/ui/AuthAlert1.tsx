2a85489fbe845632d336ed32ee6cbcdf5c3a27d3
Brandon Ramos <bramos005@citymail.cuny.edu>
2024-03-13 15:26:53 -0400

    formatted styling

--- app/components/ui/AuthAlert1.tsx
+++ app/components/ui/AuthAlert1.tsx
@@ -4,7 +4,7 @@ function AuthAlert1() {
   return (
     <div>
       <div
-        className="p-4 mb-4 text-sm text-[black] rounded-lg bg-red-500"
+        className="p-4 mb-4 text-sm text-black rounded-lg bg-red-500"
         role="alert"
       >
         <span className="font-medium">Invalid Credentials!</span> Password or Email
